ce90b49e2e315872dc7375745b8a3f83a61c1090
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2024-11-03 13:17:26 +0100

    Add PagedList from NuGet  and Car class

---
+++ DriveIt/Model/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace DriveIt.Model
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public Guid ID { get; set; }
+
+        public Car(string brand, string model, int year, Guid id)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+            ID = id;
+        }
+    }
+}
