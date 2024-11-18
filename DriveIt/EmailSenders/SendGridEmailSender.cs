5571276960fb24ed79ccda047d2045dece4f887b
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2024-11-18 23:36:02 +0100

    Add SendGridEmailSender

---
+++ DriveIt/EmailSenders/SendGridEmailSender.cs
@@ -0,0 +1,30 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace DriveIt.EmailSenders
+{
+    public class SendGridEmailSender : IGeneralEmailSender
+    {
+        private readonly string _sendGridApiKey;
+
+        public SendGridEmailSender(string sendGridApiKey)
+        {
+            _sendGridApiKey = sendGridApiKey ?? throw new ArgumentNullException(nameof(sendGridApiKey));
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var client = new SendGridClient(_sendGridApiKey);
+            var from = new EmailAddress("no-reply@driveit.com", "DriveIt");
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, message, message);
+
+            var response = await client.SendEmailAsync(msg);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new InvalidOperationException($"SendGrid error: {response.StatusCode}");
+            }
+        }
+    }
+}
