97bc47344118fde6656f2ce19ee18ef60fff6848
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2024-11-18 20:12:57 +0100

    Add parametless constructor for EF

--- DriveItAPI/Model/RentOffer.cs
+++ DriveItAPI/Model/RentOffer.cs
@@ -1,12 +1,22 @@
 ﻿namespace DriveItAPI.Model
 {
-    public record class RentOffer(Car Car)
+    public record class RentOffer
     {
-        public Guid Id { get; } = new();
-        public decimal RentPricePerDay { get; init; } = Car.RentPricePerDay;
+        public Guid Id { get; set; } = new();
+        public Car Car { get; set; }
+        public decimal RentPricePerDay { get; init; }
         //public decimal TotalPrice => RentPricePerDay * Days;
-        public decimal InsurancePriccePerDay { get; init; } = Car.InsurancePricePerDay;
+        public decimal InsurancePriccePerDay { get; init; }
         public int OfferTimeLimit { get; init; } = 10;
+
+        public RentOffer() { }
+
+        public RentOffer(Car car)
+        {
+            Car = car;
+            RentPricePerDay = car.RentPricePerDay;
+            InsurancePriccePerDay = car.InsurancePricePerDay;
+        }
 
     }
 }
