a15898e3b438a517c11922edf3445fc650d77229
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2024-11-18 18:22:34 +0100

    Add city to Car

--- DriveIt/Model/Car.cs
+++ DriveIt/Model/Car.cs
@@ -6,13 +6,15 @@
         public string Model { get; set; }
         public int Year { get; set; }
         public Guid ID { get; set; }
+        public string City { get; set; }
 
-        public Car(string brand, string model, int year, Guid id)
+        public Car(string brand, string model, int year, Guid id, string city)
         {
             Brand = brand;
             Model = model;
             Year = year;
             ID = id;
+            City = city;
         }
     }
 }
