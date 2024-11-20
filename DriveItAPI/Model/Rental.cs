6f0377146e8ae920348fa49300fe466ffdb8d1d3
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2024-11-21 00:23:15 +0100

    Improve main page layout and add filtering by city

--- DriveItAPI/Model/Rental.cs
+++ DriveItAPI/Model/Rental.cs
@@ -1,6 +1,20 @@
 ﻿namespace DriveItAPI.Model
 {
-    public record class Rental(Guid Id, Car Car, Guid UserId, DateTime StartDate)
+    public record class Rental
     {
+        public Guid Id { get; set; }
+        public Car Car { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public Rental() { }
+
+        public Rental(Guid id, Car car, Guid userId, DateTime startDate)
+        {
+            Id = id;
+            Car = car;
+            UserId = userId;
+            StartDate = startDate;
+        }
     }
 }
