acb07cec05787e391cbad33b93564bc067450214
Jan Machalski <janmachalski@outlook.com>
2025-01-15 22:22:44 +0100

    api db integration tests

---
+++ DriveItAPI.IntegrationTests/CarsIntegrationTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using DriveItAPI.Model;
+using DriveItAPI.Data;
+using DriveItAPI.IntegrationTests.Helpers;
+using System;
+using System.Linq;
+
+namespace DriveItAPI.IntegrationTests
+{
+    public class CarsIntegrationTests
+    {
+        [Fact]
+        public void AddCar_ShouldSaveToDatabase()
+        {
+            // Arrange
+            var context = InMemoryDbContextHelper.GetContext("CarsTestDb");
+            var carId = Guid.NewGuid();
+            var car = new Car(
+                brand: "Toyota",
+                model: "Corolla",
+                year: 2022,
+                id: carId,
+                available: true,
+                city: "Warsaw",
+                rentPricePerDay: 100m,
+                insurancePricePerDay: 15m
+            );
+
+            // Act
+            context.Cars.Add(car);
+            context.SaveChanges();
+
+            // Assert
+            var carFromDb = context.Cars.FirstOrDefault(c => c.Id == carId);
+            Assert.NotNull(carFromDb);
+            Assert.Equal("Toyota", carFromDb.Brand);
+            Assert.Equal(2022, carFromDb.Year);
+            Assert.True(carFromDb.Available);
+        }
+    }
+}
---
+++ DriveItAPI.IntegrationTests/InMemoryContextHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DriveItAPI.Data;
+using DriveItAPI.Model;
+
+namespace DriveItAPI.IntegrationTests.Helpers
+{
+    public static class InMemoryDbContextHelper
+    {
+        public static CarRentalContext GetContext(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<CarRentalContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            return new CarRentalContext(options);
+        }
+    }
+}
